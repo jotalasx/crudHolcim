7934fb8b05d100f788c7522b553de66132b0e3d5
jotalasx <jotalasx@gmail.com>
2019-10-25 11:19:57 +0200

    Initial commit

---
+++ src/main/java/FormType.java
@@ -0,0 +1,2 @@
+public class FormType {
+}
---
+++ src/main/java/com/example/crudHolcim/CrudHolcimApplication.java
@@ -0,0 +1,13 @@
+package com.example.crudHolcim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudHolcimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudHolcimApplication.class, args);
+	}
+
+}
